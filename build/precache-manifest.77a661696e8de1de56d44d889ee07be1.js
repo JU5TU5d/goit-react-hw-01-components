49f43c418ba643eaf98fb487f2e8f13a136138bb
JU5TU5d <51639729+JU5TU5d@users.noreply.github.com>
2020-05-20 08:49:54 +0300

    Add files via upload

---
+++ build/precache-manifest.77a661696e8de1de56d44d889ee07be1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c5560bbe68097daf4e4031e221d467",
+    "url": "/JU5TU5d/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "01019641e5dd850efeb0",
+    "url": "/JU5TU5d/goit-react-hw-01-components/static/css/main.86a7e485.chunk.css"
+  },
+  {
+    "revision": "2ae335a342baee4d88cf",
+    "url": "/JU5TU5d/goit-react-hw-01-components/static/js/2.2d80fcd4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/JU5TU5d/goit-react-hw-01-components/static/js/2.2d80fcd4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01019641e5dd850efeb0",
+    "url": "/JU5TU5d/goit-react-hw-01-components/static/js/main.9c50d0d1.chunk.js"
+  },
+  {
+    "revision": "804f2ede77606139f504",
+    "url": "/JU5TU5d/goit-react-hw-01-components/static/js/runtime-main.1855fb89.js"
+  },
+  {
+    "revision": "e631d2735799aa943d93d301abf423d2",
+    "url": "/JU5TU5d/goit-react-hw-01-components/static/media/Lato-Black.e631d273.ttf"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/JU5TU5d/goit-react-hw-01-components/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/JU5TU5d/goit-react-hw-01-components/static/media/Lato-Regular.2d36b1a9.ttf"
+  }
+]);
